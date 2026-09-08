42ac90f048969881d7531ae3614b0c8a75a994f5
Kevin Sookocheff <kevin@sookocheff.com>
2026-09-08 12:27:47 -0400

    update telescope branch

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -85,7 +85,6 @@ return {
   },
   {
     'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
     dependencies = {
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
       'nvim-telescope/telescope-ui-select.nvim',
